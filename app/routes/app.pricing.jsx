63ad11358d410a028e0eb8d9d8041526f5f92cac
Tajuddin80 <tajuddin.cse.dev@gmail.com>
2026-03-28 14:16:39 +0600

    first commit

---
+++ app/routes/app.pricing.jsx
@@ -0,0 +1,9 @@
+export default function PricingPage() {
+  return (
+    <s-page heading="Pricing page">
+      <s-section heading="Multiple pages">
+        <s-paragraph>This is pricing page made by Tajuddin</s-paragraph>
+      </s-section>
+    </s-page>
+  );
+}
